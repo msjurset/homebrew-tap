d914cd069d8de499703078c723d45a1775d49290
Mark Sjurseth <mark@sjurseth.com>
2026-03-17 09:39:25 -0400

    runbook-mac 1.0.0

--- Casks/runbook-mac.rb
+++ Casks/runbook-mac.rb
@@ -1,6 +1,6 @@
 cask "runbook-mac" do
   version "1.0.0"
-  sha256 "PLACEHOLDER"
+  sha256 "63fbadb1fe235f61087f0a12c7539cb7e096ab7351a2d75e6135205dbb9a5b3a"
 
   url "https://github.com/msjurset/runbook-mac/releases/download/v#{version}/Runbook-#{version}.zip"
   name "Runbook"
