859df3ad1d07a5ae00a9a31df43501eb324d155f
Mark Sjurseth <mark@sjurseth.com>
2026-03-18 11:18:53 -0400

    stash-mac 1.1.0

--- Casks/stash-mac.rb
+++ Casks/stash-mac.rb
@@ -1,6 +1,6 @@
 cask "stash-mac" do
-  version "1.0.0"
-  sha256 "PLACEHOLDER"
+  version "1.1.0"
+  sha256 "54fb38c0b6d2f1e42a52ba3ddb19cdc0353cb0a6f3aa8cea29e8cfb3c7b9634a"
 
   url "https://github.com/msjurset/stash-mac/releases/download/v#{version}/Stash-#{version}.zip"
   name "Stash"
