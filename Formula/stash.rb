f29c9f64facd5d6ec3377c14d7ac7868883dc677
Mark Sjurseth <mark@sjurseth.com>
2026-03-18 11:24:45 -0400

    Fix stash formula: use v0.3.0 with correct sha256, add man page

--- Formula/stash.rb
+++ Formula/stash.rb
@@ -1,8 +1,8 @@
 class Stash < Formula
   desc "Store, organize, and retrieve links, files, snippets, images, and emails"
   homepage "https://github.com/msjurset/gostash"
-  url "https://github.com/msjurset/gostash/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PLACEHOLDER"
+  url "https://github.com/msjurset/gostash/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "696872d747d967f64cf61bd3f40ae0bc07a50f36917ed70e8257077bdf061780"
   license "MIT"
 
   depends_on "go" => :build
@@ -10,6 +10,7 @@ class Stash < Formula
   def install
     ldflags = "-X main.version=#{version}"
     system "go", "build", *std_go_args(ldflags:), "./cmd/stash"
+    man1.install "stash.1"
     zsh_completion.install "completions/stash.zsh" => "_stash"
     bash_completion.install "completions/stash.bash" => "stash"
   end
